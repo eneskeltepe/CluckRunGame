f780b91348ccd74b403ea22ea35b95fe8afff0f2
Enes <102032740+eneskeltepe@users.noreply.github.com>
2025-04-03 21:05:45 +0300

    v0.15 Pause System Added

--- Assets/_GameAssets/Scripts/GamePlay/Player/PlayerController.cs
+++ Assets/_GameAssets/Scripts/GamePlay/Player/PlayerController.cs
@@ -52,6 +52,11 @@ public class PlayerController : MonoBehaviour
 
     private void Update()
     {
+        if (GameManager.Instance.GetCurrentGameState() != GameState.Play && GameManager.Instance.GetCurrentGameState() != GameState.Resume)
+        {
+            return;
+        }
+
         SetInputs();
         SetStates();
         SetPlayerDrag();
@@ -60,6 +65,11 @@ public class PlayerController : MonoBehaviour
 
     private void FixedUpdate()
     {
+        if (GameManager.Instance.GetCurrentGameState() != GameState.Play && GameManager.Instance.GetCurrentGameState() != GameState.Resume)
+        {
+            return;
+        }
+
         SetPlayerMovement();
     }
 
